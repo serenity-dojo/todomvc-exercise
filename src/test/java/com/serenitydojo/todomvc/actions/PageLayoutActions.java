db6abd032af2bab9189215f1eb50748aa7c56943
John Ferguson Smart <john.smart@wakaleo.com>
2022-07-29 08:53:53 +0100

    Sample solution

--- src/test/java/com/serenitydojo/todomvc/actions/PageLayoutActions.java
+++
@@ -1,9 +0,0 @@
-package com.serenitydojo.todomvc.actions;
-
-import net.serenitybdd.core.steps.UIInteractionSteps;
-
-public class PageLayoutActions extends UIInteractionSteps {
-    public String getHeading() {
-        return find(ApplicationHomePage.HEADING).getText();
-    }
-}
